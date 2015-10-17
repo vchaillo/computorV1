7919e7bfe29b35d124b0762e87b442b82efb8857
vchaillo <valentinchaillou89@gmail.com>
2015-10-18 01:54:50 +0200

    delta < 0 OK

---
+++ README.md
@@ -0,0 +1,9 @@
+#ComputorV1
+
+####TodoList
+- Gestion d'erreur
+- Equations du premier degre
+- Delta inferieur a zero
+- Cas particuliers
+- Check correction
+- Gestion naturel
--- solve.php
+++ solve.php
@@ -27,21 +27,43 @@
 			$sq_delta = square_root($eq['delta']);
 			$x1 = ($eq['b'] - $sq_delta) / (2 * $eq['a']);
 			$x2 = (-$eq['b'] - $sq_delta) / (2 * $eq['a']);
-
-			echo $eq['b'] - $sq_delta / (2 * $eq['a']);
+			$x1 = round($x1, 6);
+			$x2 = round($x2, 6);
 
 			echo 'x1 = b - √Δ / 2a' . "\n";
-			echo 'x1 = ' . $eq['b'] . ' - √' . $eq['delta'] . '/ 2 * ' . $eq['a'] . "\n";
+			echo 'x1 = ' . $eq['b'] . ' - √' . $eq['delta'] . ' / 2 * ' . $eq['a'] . "\n";
 			echo "\033[41m\033[1mx1 = " . $x1 . "\033[0m\n\n";
 
 			echo 'x2 = -b - √Δ / 2a' . "\n";
-			echo 'x2 = -' . $eq['b'] . ' - √' . $eq['delta'] . '/ 2 * ' . $eq['a'] . "\n";
+			echo 'x2 = -' . $eq['b'] . ' - √' . $eq['delta'] . ' / 2 * ' . $eq['a'] . "\n";
 			echo "\033[41m\033[1mx2 = " . $x2 . "\033[0m\n\n";
 		}
 		else if ($eq['delta'] < 0)
 		{
-			echo 'z1 = ' . "\n";
-			echo 'z2 = ' . "\n";
+
+			$sq_delta = square_root(-$eq['delta']);
+			$z1r = ($eq['b'] / (2 * $eq['a']));
+			$z1i = ($sq_delta / (2 * $eq['a']));
+			$z2r = -$z1r;
+			$z2i = $z1i;
+
+			echo 'z1 = b - i√Δ / 2a' . "\n";
+			echo 'z1 = ' . $eq['b'] . ' - i√' . -$eq['delta'] . ' / 2 * ' . $eq['a'] . "\n";
+			echo "\033[41m\033[1mz1 = " . $z1r;
+			if ($z1i >= 0)
+				echo ' + ';
+			else
+				echo ' - ';
+			echo $z1i . ' * i' . "\033[0m\n\n";
+
+			echo 'z2 = b - i√Δ / 2a' . "\n";
+			echo 'z2 = ' . $eq['b'] . ' - i√' . -$eq['delta'] . '/ 2 * ' . $eq['a'] . "\n";
+			echo "\033[41m\033[1mz2 = " . $z2r;
+			if ($z2i >= 0)
+				echo ' + ';
+			else
+				echo ' - ';
+			echo $z2i . ' * i' . "\033[0m\n\n";
 		}
 		else // delta == 0
 		{
